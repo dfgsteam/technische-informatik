c309d48de98e2ca74469e4d1cee49597ed668f11
Julius Hunold [mac] <info@julius-hunold.de>
2024-08-20 15:08:46 +0200

    w1/task02-3

--- w1/task02-3.s
+++ w1/task02-3.s
@@ -3,13 +3,17 @@
 .text
 
 _start:
-    li      a0, 0
-    blt     a0, zero, log
-    addi    a7, x0, 93
-    ecall
+    li      a0, 0 # k
+    li      t0, 0 # x
+    blt     zero, t0, LOG
+    j END
 
-log:
-    srli    
+LOG:
+    srli    t0, t0, 1
     addi    a0, a0, 1
+    blt     zero, t0, LOG
+    j END
+
+END:
     addi    a7, x0, 93
     ecall
